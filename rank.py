b030ea9caa23e2207ff53306345ea5abfba65536
samibey <bastabey77@gmail.com>
2024-08-19 20:43:43 +0300

    migrate to v-1.0

--- rank.py
+++ rank.py
@@ -5,7 +5,7 @@ import gradio as gr
 
 from datetime import datetime
 from pymongo import MongoClient
-#import cohere
+import cohere
 
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
@@ -14,7 +14,7 @@ import warnings
 
 warnings.filterwarnings("ignore", category=UserWarning, module="cohere")
 
-llm_model       = "gpt-4o-mini"
+llm_model       = "gemini-1.5-flash"
 embedding_model = "text-embedding-3-large"
 vec_dim         = 1024
 max_tokens      = 500
@@ -42,7 +42,7 @@ res = embed_text("apple is juicy and delicious")
 print(f"embeddings = {len(res)}\n")
 
 completion = oai.chat.completions.create(
-  model=llm_model,
+  model="gpt-4o-mini",
   messages=[
     {"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": "what is the capital of Turkey?"}
@@ -50,6 +50,161 @@ completion = oai.chat.completions.create(
 )
 
 print(f"openai: {completion.choices[0].message.content}\n")
+
+#------------------------------------------------------------------------------
+
+mongo = MongoClient(os.environ["MONGODB"])
+
+try:
+    mongo.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!\n")
+except Exception as e:
+    print(e)
+
+db = mongo.a3
+docs = db.docs
+vectors = db.vectors
+qnas = db.qnas
+logs = db.logs
+
+#win_size = 800 # sliding window size
+#win_step = 400 # sliding window step
+
+###############################################################################
+def search_similar_vectors(vectors, query_vec, top_k=top_k):
+###############################################################################
+    # Define the Atlas Search pipeline.
+    pipeline = [
+        {
+            '$vectorSearch': {
+                'index': 'vectors_index',
+                'path': 'emb',
+                'queryVector': query_vec,
+                'numCandidates': 150,
+                'limit': top_k
+            }
+        }, {
+            '$project': {
+                '_id': 1,
+                'doc_name': 1,
+                'score': {
+                    '$meta': 'vectorSearchScore'
+                },
+                'idx': 1,
+                'type': 1,
+                'text': 1
+            }
+        }
+    ]
+    # Execute the search pipeline.
+    results = list(vectors.aggregate(pipeline))
+
+    return results
+
+#------------------------------------------------------------------------------
+
+###############################################################################
+def rerank(co, query, similar_vectors, top_n=top_k):
+###############################################################################
+  # print(f"\ncohere rerank: {query}\n")
+
+  rr_docs = []
+  for doc in similar_vectors:
+    rr_docs.append(doc['text'])
+
+  res = co.rerank(model="rerank-english-v3.0", query=query, documents=rr_docs, top_n=top_n, return_documents=True)
+
+  # print the results
+  # for doc in res.results:
+  #     print(f"Doc {doc.index:03d} -> score: {doc.relevance_score:.5f} {similar_vectors[doc.index]['idx']:03d} {similar_vectors[doc.index]['type']}\t{similar_vectors[doc.index]['doc_name']}")
+
+  return res.results
+#------------------------------------------------------------------------------
+
+###############################################################################
+def get_llm_context(mongo, vec_id):
+###############################################################################
+  db = mongo.a3
+  docs = db.docs
+  vectors = db.vectors
+
+  vec = vectors.find_one({'_id': vec_id})
+  # print(f"get_llm_context: {vec['doc_name']}\t{vec['idx']}\t{vec['doc_id']}")
+  idx = vec['idx']
+
+  type = vec['type']
+  # if type == 'chunk':
+  # win_size = 400
+  win_size = 500
+  win_step = 200
+
+  pvec = db.docs.find_one({'_id': vec['doc_id']})
+  #print(pvec['name'])
+  text = pvec['text']
+
+  start_idx = idx * win_step
+  start_context = max(0, start_idx - 2 * win_size)
+  end_context = min(len(text), start_context + 4 * win_size)
+
+  return text[start_context:end_context]
+
+###############################################################################
+def rerank(co, query, similar_vectors, top_n=top_k):
+###############################################################################
+  # print(f"\ncohere rerank: {query}\n")
+
+  rr_docs = []
+  for doc in similar_vectors:
+    rr_docs.append(doc['text'])
+
+  res = co.rerank(model="rerank-english-v3.0", query=query, documents=rr_docs, top_n=top_n, return_documents=True)
+
+  # print the results
+  # for doc in res.results:
+  #     print(f"Doc {doc.index:03d} -> score: {doc.relevance_score:.5f} {similar_vectors[doc.index]['idx']:03d} {similar_vectors[doc.index]['type']}\t{similar_vectors[doc.index]['doc_name']}")
+
+  return res.results
+#------------------------------------------------------------------------------
+
+###############################################################################
+def get_llm_context(mongo, vec_id):
+###############################################################################
+  db = mongo.a3
+  docs = db.docs
+  vectors = db.vectors
+
+  vec = vectors.find_one({'_id': vec_id})
+  # print(f"get_llm_context: {vec['doc_name']}\t{vec['idx']}\t{vec['doc_id']}")
+  idx = vec['idx']
+
+  type = vec['type']
+  # if type == 'chunk':
+  win_size = 400
+  win_step = 200
+
+  win_size = 500
+  pvec = db.docs.find_one({'_id': vec['doc_id']})
+  #print(pvec['name'])
+  text = pvec['text']
+
+  start_idx = idx * win_step
+  start_context = max(0, start_idx - 2 * win_size)
+  end_context = min(len(text), start_context + 4 * win_size)
+
+  return text[start_context:end_context]
+#------------------------------------------------------------------------------
+
+genai.configure(api_key=os.environ["GEMINI"])
+
+# for m in genai.list_models():
+#   if 'generateContent' in m.supported_generation_methods:
+#     print(m.name)
+
+gm = genai.GenerativeModel(llm_model)
+
+prompt = "what is the capital of Turkey?"
+r = gm.generate_content(prompt)
+print(f"gemini: {r.text}\n")
 
 #------------------------------------------------------------------------------
 
