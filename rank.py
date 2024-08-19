67857aff3cf7da7b5ba45ceef57d989ccad14419
samibey <bastabey77@gmail.com>
2024-08-19 17:52:00 +0300

    migrate to v 1.0

--- rank.py
+++ rank.py
@@ -1,18 +1,18 @@
-from pinecone import Pinecone
 import os, json, datetime, time
 from openai import OpenAI
 from fastapi import FastAPI
 import gradio as gr
-## github
 
 from datetime import datetime
 from pymongo import MongoClient
-import cohere
+#import cohere
 
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
+import warnings
 
+warnings.filterwarnings("ignore", category=UserWarning, module="cohere")
 
 llm_model       = "gpt-4o-mini"
 embedding_model = "text-embedding-3-large"
@@ -26,9 +26,36 @@ oai = OpenAI(
     api_key = os.environ["OPENAI"],
  )
 
+###############################################################################
+def embed_text(text):
+###############################################################################
+  res = oai.embeddings.create(
+                input=text,
+                model=embedding_model,
+                dimensions=vec_dim
+              )
+  return res.data[0].embedding
+#------------------------------------------------------------------------------
+
+res = embed_text("apple is juicy and delicious")
+
+print(f"embeddings = {len(res)}\n")
+
+completion = oai.chat.completions.create(
+  model=llm_model,
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "what is the capital of Turkey?"}
+  ]
+)
+
+print(f"openai: {completion.choices[0].message.content}\n")
+
+#------------------------------------------------------------------------------
+
 # answer = "Please refer to the OSA webpage: https://www.aub.edu.lb/SAO/Pages/default.aspx"
 
-def qna():
+def qna(question="hello")->str:
        return ("hello qna")
 
 iface = gr.Interface(
